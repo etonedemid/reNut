3095441c64fa09ca30150ac20b62f41e18441779
MadLadMikael <masterspike52@gmail.com>
2026-03-12 13:53:13 -0500

    Update hooks.cpp

--- src/renut_engine/hooks.cpp
+++ src/renut_engine/hooks.cpp
@@ -10,8 +10,9 @@
 
 REXCVAR_DEFINE_BOOL(overworld_vehicles, false, "Nuts&Bolts", "Enables Overworld Vehicles");
 REXCVAR_DEFINE_BOOL(no_notes_spent, false, "Nuts&Bolts", "hook created by serenity");
-REXCVAR_DEFINE_INT32(fpsvsync, 0, "Nuts&Bolts", "Immediate (0) 60Hz (1) 30Hz (2) VSync = 30Hz, Threshold 20 (7)");
-//REXCVAR_DEFINE_DOUBLE(fpsCount, 0.0, "Nuts&Bolts", ""); //This would be a frame timer display that updates every frame
+REXCVAR_DEFINE_INT32(fpsvsync, 0, "Nuts&Bolts", "Immediate (0) 60Hz (1) 30Hz (2) VSync = 30Hz, Threshold 20 (7)")
+.range(0, 7)
+.lifecycle(rex::cvar::Lifecycle::kRequiresRestart);
 
 
 
