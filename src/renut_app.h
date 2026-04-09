6f691a2479a96f9d687e412abf4caf2f53cd2055
MadLadMikael <masterspike52@gmail.com>
2026-04-09 06:30:23 -0500

    .

--- src/renut_app.h
+++ src/renut_app.h
@@ -5,9 +5,9 @@
 #pragma once
 #include <rex/rex_app.h>
 #include "renut_engine/overlays/fps_overlay_dialog.h"
-#include "renut_engine/overlays/renut_logging_overlay.h"
 #include "renut_engine/sleep.h"
-
+#include "renut_engine/renut_logging.h"
+#include "renut_engine/overlays/renut_logging_overlay.h"
 
 
 class RenutApp : public rex::ReXApp {
@@ -18,11 +18,6 @@ public:
         return std::unique_ptr<RenutApp>(new RenutApp(ctx, "renut",
             PPCImageConfig));
     }
-    // Override virtual hooks for customization:
-    // void OnPreSetup(rex::RuntimeConfig& config) override {}
-    // void OnPostSetup() override {}
-    // void OnShutdown() override {}
-    // void OnConfigurePaths(rex::PathConfig& paths) override {}
 
     void OnCreateDialogs(rex::ui::ImGuiDrawer* drawer) override {
         drawer->AddDialog(new FpsOverlayDialog(drawer));
@@ -32,8 +27,4 @@ public:
     void OnShutdown() override {
         DisableHighResTimer();
     }
-    //void OnConfigurePaths(rex::PathConfig& paths) override {
-    //    // Redirect game data root from assets/debug to assets/bundle
-    //    paths.game_data_root = paths.game_data_root.parent_path() / "assets";
-    //}
 };
