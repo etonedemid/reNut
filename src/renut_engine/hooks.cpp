c804607409d82b16230491ffd7e2dea0b8dbcb0c
etonedemid <3damir0n3@gmail.com>
2026-04-12 13:43:40 -0300

    reverts

--- src/renut_engine/hooks.cpp
+++ src/renut_engine/hooks.cpp
@@ -148,3 +148,17 @@ bool BanjoActorOverride(PPCRegister& r3, PPCRegister& r5) {
     // "default" — let the original function run
     return true;
 }
+
+void klungofps() {
+    if (REXCVAR_GET(target_fps) > 30) {
+        REXCVAR_GET(target_fps) = 30;
+        return;
+    }
+}
+
+void klungofps2() {
+    if (REXCVAR_GET(target_fps) == 30) {
+        REXCVAR_GET(target_fps) = 60;
+        return;
+    }
+}
